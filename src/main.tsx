8ff61f1f0be1b52200cbaca62d35f2549e57ae10
Fabrício <fabriciolak12@gmail.com>
2023-01-27 19:23:57 -0300

    feat: add theme provider from styled-components

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
