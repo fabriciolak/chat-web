b5a6e3562f04c3f2d6ae4356244e8fdec84477f4
Fabrício <fabriciolak12@gmail.com>
2023-01-27 20:23:46 -0300

    feat: add panel component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
+import { Panel } from "./components/Panel"
 import { GlobalStyle } from "./styles/global"
 import { defaultTheme } from "./styles/themes/default"
 
 export function App() {
   return (
     <>
-      <div>
-        <h1>Hello</h1>
-
-      </div>
-      
+      <Panel />
       <GlobalStyle theme={defaultTheme} />
     </>
   )
