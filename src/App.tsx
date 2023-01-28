ee517df8e8f727ef97ed8a1f2f4df03d654440da
Fabrício <fabriciolak12@gmail.com>
2023-01-28 00:08:04 -0300

    feat: use panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
+import { DotsThreeVertical, PhoneCall, Plus } from "phosphor-react"
+import { Button } from "./components/Button"
+
+import { Chat } from "./components/Chat"
 import { Panel } from "./components/Panel"
+import { Conversations, SearchContainer } from "./components/Panel/styles"
+
 import { GlobalStyle } from "./styles/global"
 import { defaultTheme } from "./styles/themes/default"
 
 export function App() {
   return (
-    <>
-      <Panel />
+    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <Panel title="Messages" badge={true} badgeValue={12} badgeBackground="#EDF2F7">
+        <SearchContainer>
+          <input type="text" placeholder='Search messages' />
+        </SearchContainer>
+
+        <Conversations>  
+          <Chat />
+          <Chat />
+          <Chat />
+        </Conversations>
+      </Panel>
+
+      <Panel title="Members">
+        <Conversations>  
+          <Chat />
+          <Chat />
+          <Chat />
+        </Conversations>        
+      </Panel>
+
       <GlobalStyle theme={defaultTheme} />
-    </>
+    </div>
   )
 }
 
